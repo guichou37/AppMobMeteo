5ad9bdb5cc8c8f44ab6979a5c6686366fd52d4cf
Guyard Alexandre <alexandre.guyard076@laposte.net>
2016-01-09 17:32:11 +0100

    Refacture de l'architecture.

--- app/src/main/java/com/example/appweb/myapplication/MapsActivity.java
+++ app/src/main/java/com/example/appweb/myapplication/MapsActivity.java
@@ -1,20 +1,14 @@
 package com.example.appweb.myapplication;
 
-import android.app.AlertDialog;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.location.Location;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
-import android.os.AsyncTask;
+
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.widget.TextView;
+import android.util.Log;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -27,12 +21,6 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 
 public class MapsActivity extends FragmentActivity implements LocationListener, OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
@@ -58,72 +46,7 @@ public class MapsActivity extends FragmentActivity implements LocationListener, 
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-        //Création du ViewPager
-        mPager = (ViewPager) findViewById(R.id.pager);
-        mPagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
-        mPager.setAdapter(mPagerAdapter);
-
-        //Vérification de la connexion
-        ConnectivityManager connMgr = (ConnectivityManager)
-                getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
-
-        if (networkInfo != null && networkInfo.isConnected()) {
-            HTTPrequests req = new HTTPrequests();
-            req.execute();
-        } else {
-            alerte("Pas de connexion");
-        }
     }
-
-    //region ViewPager
-
-    private ViewPager mPager;
-    private ScreenSlidePagerAdapter mPagerAdapter;
-    private static final int NUM_PAGES =3;
-
-    private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
-        public ScreenSlidePagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            switch (position){
-                case 0: return new ScreenSlidePageFragment();
-                case 1: return new ScreenSlidePageFragment2();
-                case 2: return new ScreenSlidePageFragment3();
-                default:
-                    return null;
-            }
-        }
-
-        @Override
-        public int getCount() {
-            return NUM_PAGES;
-        }
-    }
-
-    public void updatefragments_slides(){
-        if(mPager.getCurrentItem()==0) {
-            TextView latitudeset = (TextView) findViewById(R.id.mLatitudeText);
-            latitudeset.setText(String.valueOf(latitude));
-            TextView longitudeset = (TextView) findViewById(R.id.mLongitudeText);
-            longitudeset.setText(String.valueOf(longitude));
-            TextView JSONset = (TextView) findViewById(R.id.mJSON);
-            JSONset.setText(JSONTEST);
-        }
-        else if(mPager.getCurrentItem()==1)
-        {
-
-        }
-        else if(mPager.getCurrentItem()==2)
-        {
-
-        }
-    }
-
-    //endregion
 
     //region GoogleMap
 
@@ -156,6 +79,11 @@ public class MapsActivity extends FragmentActivity implements LocationListener, 
             longitude=mLastLocation.getLongitude();
         }
         LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+
+        //Création du ViewPager
+        mPager = (ViewPager) findViewById(R.id.pager);
+        mPagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
+        mPager.setAdapter(mPagerAdapter);
     }
 
     @Override
@@ -173,83 +101,42 @@ public class MapsActivity extends FragmentActivity implements LocationListener, 
         mylocation = location;
         latitude=mylocation.getLatitude();
         longitude= mylocation.getLongitude();
-
-        HTTPrequests req = new HTTPrequests();
-        req.execute();
-        updatefragments_slides();
-
         LatLng movelocation = new LatLng(latitude, longitude);
         mMap.moveCamera(CameraUpdateFactory.newLatLng(movelocation));
+        Log.d("debug", latitude + " " + longitude);
     }
 
     //endregion
 
-    //region OpenWeatherMap
-    String JSONTEST = new String();
+    //region ViewPager
 
-    private class HTTPrequests extends AsyncTask<Void, Integer, String> {
+    private ViewPager mPager;
+    private ScreenSlidePagerAdapter mPagerAdapter;
+    private static final int NUM_PAGES =3;
 
-        String response;
-
-        @Override
-        protected String doInBackground(Void... params) {
-
-            String request = "http://api.openweathermap.org/data/2.5/weather?lat="+String.valueOf(latitude)+"&lon="+String.valueOf(longitude)+"&APPID=d63e568fa914f6354620fe3481c3921c";
-
-            try {
-                response = sendGet(request);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            return response;
+    private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
+        public ScreenSlidePagerAdapter(FragmentManager fm) {
+            super(fm);
         }
 
         @Override
-        protected void onPostExecute(String result) {
-            JSONTEST = response;
+        public Fragment getItem(int position) {
+            switch (position){
+                case 0: return ScreenSlidePageFragment.newInstance(0,latitude,longitude);
+                case 1: return ScreenSlidePageFragment.newInstance(1,latitude,longitude);
+                case 2: return ScreenSlidePageFragment.newInstance(2,latitude,longitude);
+                default:
+                    return null;
+            }
         }
 
-        private String sendGet(String url) throws Exception {
-
-            StringBuffer chaine = new StringBuffer("");
-            try {
-                URL address = new URL(url);
-                HttpURLConnection connection = (HttpURLConnection) address.openConnection();
-                connection.setRequestProperty("User-Agent", "");
-                connection.setRequestMethod("GET");
-                connection.setDoInput(true);
-                connection.connect();
-
-                InputStream inputStream = connection.getInputStream();
-
-                BufferedReader rd = new BufferedReader(new InputStreamReader(inputStream));
-                String line = "";
-                while ((line = rd.readLine()) != null) {
-                    chaine.append(line);
-                }
-
-            } catch (IOException e) {
-                // writing exception to log
-                e.printStackTrace();
-            }
-
-            return chaine.toString();
-
+        @Override
+        public int getCount() {
+            return NUM_PAGES;
         }
     }
 
     //endregion
 
-    private void alerte (String alerttext){
-        AlertDialog alertDialog = new AlertDialog.Builder(MapsActivity.this).create();
-        alertDialog.setTitle("Alerte");
-        alertDialog.setMessage(alerttext);
-        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
-                new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.dismiss();
-                    }
-                });
-        alertDialog.show();
-    }
+
 }
--- app/src/main/java/com/example/appweb/myapplication/ScreenSlidePageFragment.java
+++ app/src/main/java/com/example/appweb/myapplication/ScreenSlidePageFragment.java
@@ -1,17 +1,158 @@
 package com.example.appweb.myapplication;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
+
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class ScreenSlidePageFragment extends Fragment {
 
+    private int day;
+    private double latitude;
+    private double longitude;
+    TextView latitudeset;
+    TextView longitudeset;
+    TextView JSONset;
+
+    public static ScreenSlidePageFragment newInstance(int p, double l1, double l2){
+        ScreenSlidePageFragment sspf = new ScreenSlidePageFragment(); //Crée une nouvelle instance de la classe
+        Bundle args = new Bundle(); //Crée un bundle
+        args.putInt("day", p); //Place l'int dans le bundle
+        args.putDouble("latitude", l1);
+        args.putDouble("longitude", l2);
+        sspf.setArguments(args); //Envoie le bundle dans l'instance, ouvert juste après dans le onCreate
+        return sspf;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        day = getArguments().getInt("day"); //Récupération du paramètre stocké dans le bundle
+        latitude = getArguments().getDouble("latitude");
+        longitude = getArguments().getDouble("longitude");
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
+        //Création de la vue
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_slide, container, false);
+
+        //Vérification de la connexion
+        ConnectivityManager connMgr = (ConnectivityManager)
+                this.getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+        if (networkInfo != null && networkInfo.isConnected()) {
+            HTTPrequests req = new HTTPrequests();
+            req.execute(); //Lancement de la requête
+        } else {
+            alerte("Pas de connexion");
+        }
+
+        latitudeset = (TextView) rootView.findViewById(R.id.mLatitudeText);
+        longitudeset = (TextView) rootView.findViewById(R.id.mLongitudeText);
+        JSONset = (TextView) rootView.findViewById(R.id.mJSON);
+
         return rootView;
+    }
+
+    //region OpenWeatherMap
+    String JSONTEST;
+
+    private class HTTPrequests extends AsyncTask<Void, Integer, String> { //L'AsyncTask est nécessaire pour lancer les requêtes en fond.
+
+        String response;
+
+        @Override
+        protected String doInBackground(Void... params) {
+            String request;
+            if(day==0) {
+                request = "http://api.openweathermap.org/data/2.5/weather?lat=" + String.valueOf(latitude) + "&lon=" + String.valueOf(longitude) + "&APPID=d63e568fa914f6354620fe3481c3921c";
+            }else{
+                request = "http://api.openweathermap.org/data/2.5/forecast?lat=" + String.valueOf(latitude) + "&lon=" + String.valueOf(longitude) + "&APPID=d63e568fa914f6354620fe3481c3921c";
+            }
+
+            try {
+                response = sendGet(request);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return response;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            JSONTEST = response;
+            latitudeset.setText(String.valueOf(latitude));
+            longitudeset.setText(String.valueOf(longitude));
+            JSONset.setText(JSONTEST);
+        }
+
+        private String sendGet(String url) throws Exception {
+
+            StringBuffer chaine = new StringBuffer("");
+            try {
+                URL address = new URL(url);
+                HttpURLConnection connection = (HttpURLConnection) address.openConnection();
+                connection.setRequestProperty("User-Agent", "");
+                connection.setRequestMethod("GET");
+                connection.setDoInput(true);
+                connection.connect();
+
+                InputStream inputStream = connection.getInputStream();
+
+                BufferedReader rd = new BufferedReader(new InputStreamReader(inputStream));
+                String line;
+                while ((line = rd.readLine()) != null) {
+                    chaine.append(line);
+                }
+
+            } catch (IOException e) {
+                // writing exception to log
+                e.printStackTrace();
+            }
+
+            return chaine.toString();
+
+        }
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        Log.d("debug","vue"+day+"detruite");
+    }
+
+    //endregion
+
+    private void alerte (String alerttext){
+        AlertDialog alertDialog = new AlertDialog.Builder(this.getActivity()).create();
+        alertDialog.setTitle("Alerte");
+        alertDialog.setMessage(alerttext);
+        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+        alertDialog.show();
     }
 
 }
