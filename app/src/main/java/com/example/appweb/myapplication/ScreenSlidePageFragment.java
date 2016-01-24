5e5917086f6a40afab244b9043eeeae09e472492
Guyard Alexandre <alexandre.guyard076@laposte.net>
2016-01-24 20:19:03 +0100

    mise à jours de la map et ajout des icones meteo

--- app/src/main/java/com/example/appweb/myapplication/ScreenSlidePageFragment.java
+++ app/src/main/java/com/example/appweb/myapplication/ScreenSlidePageFragment.java
@@ -13,6 +13,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import org.json.JSONArray;
@@ -25,16 +26,28 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Calendar;
 
 public class ScreenSlidePageFragment extends Fragment {
 
     private int day;
     private double latitude;
     private double longitude;
-    TextView latitudeset;
-    TextView longitudeset;
-    TextView meteoset;
+    TextView cityView;
+    TextView dayView;
+    ImageView iconView;
+    //region Views
+    ImageView icon0;
+    ImageView icon3;
+    ImageView icon6;
+    ImageView icon9;
+    ImageView icon12;
+    ImageView icon15;
+    ImageView icon18;
+    ImageView icon21;
+    //endregion
     JSONObject result_json;
+    String iconsrc;
 
     public static ScreenSlidePageFragment newInstance(int p, double l1, double l2){
         ScreenSlidePageFragment sspf = new ScreenSlidePageFragment(); //Crée une nouvelle instance de la classe
@@ -58,10 +71,30 @@ public class ScreenSlidePageFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
         //Création de la vue
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_slide, container, false);
-
-        latitudeset = (TextView) rootView.findViewById(R.id.mLatitudeText);
-        longitudeset = (TextView) rootView.findViewById(R.id.mLongitudeText);
-        meteoset = (TextView) rootView.findViewById(R.id.meteo);
+        //region Call Views
+        cityView = (TextView) rootView.findViewById(R.id.cityView);
+        iconView = (ImageView) rootView.findViewById(R.id.iconView);
+        dayView = (TextView) rootView.findViewById(R.id.dayView);
+        icon0 = (ImageView) rootView.findViewById(R.id.img0);
+        icon3 = (ImageView) rootView.findViewById(R.id.img3);
+        icon6 = (ImageView) rootView.findViewById(R.id.img6);
+        icon9 = (ImageView) rootView.findViewById(R.id.img9);
+        icon12 = (ImageView) rootView.findViewById(R.id.img12);
+        icon15 = (ImageView) rootView.findViewById(R.id.img15);
+        icon18 = (ImageView) rootView.findViewById(R.id.img18);
+        icon21 = (ImageView) rootView.findViewById(R.id.img21);
+        //endregion
+        switch (day)
+        {
+            case 0 : dayView.setText("Aujourd'hui");
+            break;
+            case 1 : dayView.setText("Demain");
+                break;
+            case 2 : dayView.setText("Après-demain");
+                break;
+            default:
+            break;
+        }
 
         //Vérification de la connexion
         ConnectivityManager connMgr = (ConnectivityManager)
@@ -76,6 +109,7 @@ public class ScreenSlidePageFragment extends Fragment {
         }
         return rootView;
     }
+
 
     //region OpenWeatherMap
 
@@ -97,18 +131,82 @@ public class ScreenSlidePageFragment extends Fragment {
 
         @Override
         protected void onPostExecute(String result) {
+
+
+                Calendar c = Calendar.getInstance();
+                int hours = c.get(Calendar.HOUR_OF_DAY);
+                int decalage = (int) Math.floor(hours / 3);
+                int d = (8-decalage);
+
             try {
                 result_json = new JSONObject(result);
-                JSONArray list_json = result_json.getJSONArray("list");
-                JSONObject today = list_json.getJSONObject(day*8);
-                JSONArray weather_json = today.getJSONArray("weather");
-                JSONObject weather_json0 = weather_json.getJSONObject(0);
-                meteoset.setText(weather_json0.getString("description"));
-            } catch (JSONException e) {
+                JSONObject city = result_json.getJSONObject("city");
+                cityView.setText(city.getString("name"));
+                setIcons(iconView,result_json, day*8);
+            }
+            catch (JSONException e)
+            {
                 e.printStackTrace();
             }
-            latitudeset.setText(String.valueOf(latitude));
-            longitudeset.setText(String.valueOf(longitude));
+
+                if (day==0)
+                {
+                    int i=8;
+                    while(d!=0)
+                    {
+                        d=d-1;
+                        switch (i){
+                            case 8 : setIcons(icon21,result_json,i-1-decalage);
+                                break;
+                            case 7 : setIcons(icon18,result_json,i-1-decalage);
+                                break;
+                            case 6 : setIcons(icon15,result_json,i-1-decalage);
+                                break;
+                            case 5 : setIcons(icon12,result_json,i-1-decalage);
+                                break;
+                            case 4 : setIcons(icon9,result_json,i-1-decalage);
+                                break;
+                            case 3 : setIcons(icon6,result_json,i-1-decalage);
+                                break;
+                            case 2 : setIcons(icon3,result_json,i-1-decalage);
+                                break;
+                            case 1 : setIcons(icon0,result_json,i-1-decalage);
+                                break;
+                        }
+                        i--;
+
+                    }
+
+                    }
+
+                else if (day!=0)
+                {
+                    int i=8;
+                    while(i!=0)
+                    {
+                        switch (i){
+                            case 8 : setIcons(icon21,result_json,i-1+day*8-decalage);
+                                break;
+                            case 7 : setIcons(icon18,result_json,i-1+day*8-decalage);
+                                break;
+                            case 6 : setIcons(icon15,result_json,i-1+day*8-decalage);
+                                break;
+                            case 5 : setIcons(icon12,result_json,i-1+day*8-decalage);
+                                break;
+                            case 4 : setIcons(icon9,result_json,i-1+day*8-decalage);
+                                break;
+                            case 3 : setIcons(icon6,result_json,i-1+day*8-decalage);
+                                break;
+                            case 2 : setIcons(icon3,result_json,i-1+day*8-decalage);
+                                break;
+                            case 1 : setIcons(icon0,result_json,i-1+day*8-decalage);
+                                break;
+                        }
+                        i--;
+                    }
+
+                }
+
         }
 
         private String sendGet(String url) throws Exception {
@@ -143,7 +241,7 @@ public class ScreenSlidePageFragment extends Fragment {
     @Override
     public void onDestroyView() {
         super.onDestroyView();
-        Log.d("debug","vue"+day+"detruite");
+        Log.d("debug", "vue" + day + "detruite");
     }
 
     //endregion
@@ -159,6 +257,61 @@ public class ScreenSlidePageFragment extends Fragment {
                     }
                 });
         alertDialog.show();
+    }
+
+    public void setIcons(ImageView imageView , JSONObject json, int n){
+        if(n>24){imageView.setImageResource(R.drawable.inone);return;}
+        String src = null;
+        try {
+            JSONArray list_json = json.getJSONArray("list");
+            JSONObject today = list_json.getJSONObject(n);
+            JSONArray weather_json = today.getJSONArray("weather");
+            JSONObject weather_json0 = weather_json.getJSONObject(0);
+            src = weather_json0.getString("icon");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        switch(src){
+            case "01d" : imageView.setImageResource(R.drawable.i01d);
+                break;
+            case "01n" : imageView.setImageResource(R.drawable.i01n);
+                break;
+            case "02d" : imageView.setImageResource(R.drawable.i02d);
+                break;
+            case "02n" : imageView.setImageResource(R.drawable.i02n);
+                break;
+            case "03d" : imageView.setImageResource(R.drawable.i03d);
+                break;
+            case "03n" : imageView.setImageResource(R.drawable.i03n);
+                break;
+            case "04d" : imageView.setImageResource(R.drawable.i04d);
+                break;
+            case "04n" : imageView.setImageResource(R.drawable.i04n);
+                break;
+            case "09d" : imageView.setImageResource(R.drawable.i09d);
+                break;
+            case "09n" : imageView.setImageResource(R.drawable.i09n);
+                break;
+            case "10d" : imageView.setImageResource(R.drawable.i10d);
+                break;
+            case "10n" : imageView.setImageResource(R.drawable.i10n);
+                break;
+            case "11d" : imageView.setImageResource(R.drawable.i11d);
+                break;
+            case "11n" : imageView.setImageResource(R.drawable.i11n);
+                break;
+            case "13d" : imageView.setImageResource(R.drawable.i13d);
+                break;
+            case "13n" : imageView.setImageResource(R.drawable.i13n);
+                break;
+            case "50d" : imageView.setImageResource(R.drawable.i50d);
+                break;
+            case "50n" : imageView.setImageResource(R.drawable.i50n);
+                break;
+            default : imageView.setImageResource(R.drawable.inone);
+                break;
+        }
     }
 
 }
